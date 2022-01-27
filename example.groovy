7e7bee5fd89bcdeabf2599b805f15d8ded1cc474
dadaher <87011454+dadaher@users.noreply.github.com>
2022-01-27 14:34:16 +0200

    add get successful build

--- example.groovy
+++ example.groovy
@@ -5,5 +5,16 @@ def example1() {
 def example2() {
   println 'Hello from example2'
 }
+def getSuccessfulBuild() {
+def builds = []
 
+def job = jenkins.model.Jenkins.instance.getItem(JOB-NAME)
+job.builds.each {
+    if (it.result == hudson.model.Result.SUCCESS) {
+        builds.add(it.displayName[1..-1])
+    }
+}
+
+return builds
+}
 return this

812d11ad0ea0e89c52830587638e5d08a4a16764
dadaher <87011454+dadaher@users.noreply.github.com>
2022-01-27 16:15:03 +0200

    add jobname

--- example.groovy
+++ example.groovy
@@ -5,5 +5,16 @@ def example1() {
 def example2() {
   println 'Hello from example2'
 }
-
+def getSuccessfulBuild() {
+def builds = []
+def JOBNAME = "test"
+def job = jenkins.model.Jenkins.instance.getItem(JOBNAME)
+job.builds.each {
+    if (it.result == hudson.model.Result.SUCCESS) {
+        builds.add(it.displayName[1..-1])
+    }
+}
+println builds
+return builds
+}
 return this
